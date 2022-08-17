9f4f49cd2cc6791ccacc191ef2de0602441ac837
tnjobaya <tnjobaya@kinghan.com>
2022-08-17 15:39:59 +0800

    0817. HW1

---
+++ src/components/Expenses.js
@@ -0,0 +1,18 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          date={expense.date}
+          title={expense.title}
+          price={expense.amount}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;
